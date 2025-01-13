0f5734dfab7e7e2d102c9f20b4b52340e56882ee
Abhi-at-josh <abhijeet.lokhande@joshsoftware.com>
2025-01-13 23:02:18 +0530

    The Question3 to print the a-z in reverse order

--- Question3.rb
+++ Question3.rb
@@ -1,7 +1,7 @@
 #print a-z in reverse order in ruby 
 alphabet=('a'..'z').to_a.reverse.join #concate all the letters from a-z in reverse order in string
 
-puts alphabet  #Display the string
+puts alphabet                         #Display the string
 
 alphabet.each_char do |letter|      #iterate through a string 
   puts letter                       #Display each letter on new line
