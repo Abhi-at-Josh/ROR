db8f6875bd0bfc38a826ae862f62e6d1c2765795
Abhi-at-josh <abhijeet.lokhande@joshsoftware.com>
2025-01-23 15:34:55 +0530

    Add validation to IP Address before checking class

--- Question5.rb
+++ Question5.rb
@@ -1,23 +1,13 @@
-# Write a program to find IP address class.
-=begin
- The class of ip address is determined by the first octet of the address
- Class A=  1:0:0:0 to 127.255.255.255
- Class B=128:0:0:0 to 191.255.255.255
- Class C=192:0:0:0 to 223.255.255.255
- Class D=224:0:0:0 to 239.255.255.255
- Class C=240:0:0:0 to 255.255.255.255
-=end
-
 
 def find_ip_address_class(ip)
-   octets=ip.split(".")                              #split the string of ip address from "."
-
-   if octets.length !=4
+   if ip=~ /\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b/ #validate the ip address 
+      octets=ip.split(".")     #split the string of ip address from "."
+   else
       return "Invlaid IP Address"
    end
 
 
-   first_octet=octets[0].to_i                      #convert the octet to interger to check its class
+   first_octet=octets[0].to_i  #convert the octet to interger to check its class
    
    case first_octet
    when 1..127
