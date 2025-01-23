cab8cc93c17c6775170fbda1ab870260f4187b33
Abhi-at-josh <abhijeet.lokhande@joshsoftware.com>
2025-01-23 15:16:47 +0530

    Remove blank space and add end line

--- Question3.rb
+++ Question3.rb
@@ -1,8 +1,6 @@
 #print a-z in reverse order in ruby 
 alphabet=('a'..'z').to_a.reverse.join #concate all the letters from a-z in reverse order in string
-
 puts alphabet                         #Display the string
-
 alphabet.each_char do |letter|      #iterate through a string 
   puts letter                       #Display each letter on new line
 end                                 #End the loop
